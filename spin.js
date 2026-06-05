8c3f2accdf93ffdb18bb3eee22119fe67b1e7afa
Atharva Awasthi <atharva123aa@gmail.com>
2026-06-05 11:13:36 +0530

    Update spin.js

--- spin.js
+++ spin.js
@@ -1,5 +1,14 @@
 function rotateFunction(){
-    var deg=Math.floor(Math.random() *9999); 
-    document.getElementById('box').style.transform="rotate("+deg+"deg)"  ;
-    //  tried random feature forwheel of spin and yes i used ai for help as i am begginer in js
-    document.getElementById('mainbox').classList.add('animate');} 
+    var min=1024; 
+    var max=9999; 
+    var deg= Math.floor(Math.random() * (max-min)) + min;
+    document.getElementById('box').style.transform=  "rotate("+deg+"deg)";
+}
+
+
+var element= document.getElementById('mainbox');
+element.classList.remove('animate'); 
+
+ setTimeout(function(){
+    element.classList.add('animate');
+ }, 5000);
