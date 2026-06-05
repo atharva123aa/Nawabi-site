ec4f474e2fd2bc178b7c2933ce02e75224a415ff
Atharva Awasthi <atharva123aa@gmail.com>
2026-06-05 09:27:25 +0530

    Spin wheel added

---
+++ spin.js
@@ -0,0 +1,5 @@
+function rotateFunction(){
+    var deg=Math.floor(Math.random() *9999); 
+    document.getElementById('box').style.transform="rotate("+deg+"deg)"  ;
+    //  tried random feature forwheel of spin and yes i used ai for help as i am begginer in js
+    document.getElementById('mainbox').classList.add('animate');} 
